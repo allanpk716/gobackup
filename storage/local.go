e3f72d3ed2052ee803e00e0920890548541818f8
716 <525223688@qq.com>
2020-10-15 14:12:50 +0800

    修复 windows 下文件操作相关代码
    Signed-off-by: 716 <525223688@qq.com>

--- storage/local.go
+++ storage/local.go
@@ -1,9 +1,14 @@
 package storage
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"runtime"
+
 	"github.com/huacnlee/gobackup/helper"
 	"github.com/huacnlee/gobackup/logger"
-	"path"
 )
 
 // Local storage
@@ -24,15 +29,67 @@ func (ctx *Local) open() (err error) {
 func (ctx *Local) close() {}
 
 func (ctx *Local) upload(fileKey string) (err error) {
-	_, err = helper.Exec("cp", ctx.archivePath, ctx.destPath)
-	if err != nil {
-		return err
+
+	sysType := runtime.GOOS
+	if sysType == "windows" {
+		// windows系统
+		logger.Info("Windows upload")
+		logger.Info("ctx.archivePath", ctx.archivePath)
+		logger.Info("ctx.destPath", ctx.destPath)
+		logger.Info("fileKey", fileKey)
+		err := MoveFile(ctx.archivePath, path.Join(ctx.destPath, fileKey))
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err = helper.Exec("cp", ctx.archivePath, ctx.destPath)
+		if err != nil {
+			return err
+		}
 	}
+
 	logger.Info("Store successed", ctx.destPath)
 	return nil
 }
 
 func (ctx *Local) delete(fileKey string) (err error) {
-	_, err = helper.Exec("rm", path.Join(ctx.destPath, fileKey))
+	sysType := runtime.GOOS
+	if sysType == "windows" {
+		// windows系统
+		logger.Info("Windows delete")
+		logger.Info("ctx.archivePath", ctx.archivePath)
+		logger.Info("ctx.destPath", ctx.destPath)
+		logger.Info("fileKey", fileKey)
+		err := os.Remove(path.Join(ctx.destPath, fileKey))
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err = helper.Exec("rm", path.Join(ctx.destPath, fileKey))
+	}
 	return
+}
+
+func MoveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("Couldn't open dest file: %s", err)
+	}
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+	// The copy was successful, so now delete the original file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed removing original file: %s", err)
+	}
+	return nil
 }
